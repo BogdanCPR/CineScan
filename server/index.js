946ba6dfd1010cab6e471f8d1c22c06244a33d42
Bogdan Caprita <capritabogdan@yahoo.com>
2024-04-28 10:23:55 +0300

    prepare for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
-import cors from "cors"
+import helmet from "helmet"
 import http from "http"
 import mongoose from "mongoose"
 import "dotenv/config"
@@ -8,11 +8,10 @@ import routes from "./src/routes/index.js"
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(helmet());
 app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
